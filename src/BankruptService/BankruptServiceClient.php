58c8f222ceb5096a8d080e25b4695dfa5e1ef8b4
vvvizard <vvvizard@gmail.com>
2025-03-14 15:10:12 +0700

    add new methods for messages

--- src/BankruptService/BankruptServiceClient.php
+++ src/BankruptService/BankruptServiceClient.php
@@ -1,73 +1,92 @@
 <?php
+
 namespace FedResSdk\BankruptService;
 
 use FedResSdk\Authorization\Authorization;
 use FedResSdk\ClientFedRes;
 use FedResSdk\Config;
 
-class  BankruptServiceClient extends ClientFedRes {
-    
-    public const TYPE = 'BankruptService';
-    public $sort = "DatePublish:asc";
+class  BankruptServiceClient extends ClientFedRes
+{
 
-    public function __construct(Authorization $auth){
-   
-         parent::__construct($auth);
-         
-         $type = self::TYPE;
-         if($auth->getMode() == 'develop'){
-          $type = self::TYPE . 'Test';
-         }
+  public const TYPE = 'BankruptService';
+  public $sort = "DatePublish:asc";
 
-         $this->mainUrl = Config::getMainUrl($type);     
-         $this->setYesterdayDate();
-         $this->setMessagesType('ArbitralDecree');
+  public function __construct(Authorization $auth)
+  {
+
+    parent::__construct($auth);
+
+    $type = self::TYPE;
+    if ($auth->getMode() == 'develop') {
+      $type = self::TYPE . 'Test';
     }
-    
-    public function auth(){
-      if(!$this->isAuthorized()){
 
-        $this->body = $this->auth->getAuthDataJson();
-        $response = $this->apiRequest("POST", 'v1/auth');
-        if($response){
-            $data = json_decode($response, true);
-            $this->auth->storeToken($data['jwt']);
-            $this->headers['Authorization'] = 'Bearer ' . $data['jwt'];
-         }
+    $this->mainUrl = Config::getMainUrl($type);
+    $this->setYesterdayDate();
+    $this->setMessagesType('ArbitralDecree');
+  }
+
+  public function auth()
+  {
+    if (!$this->isAuthorized()) {
+
+      $this->body = $this->auth->getAuthDataJson();
+      $response = $this->apiRequest("POST", 'v1/auth');
+      if ($response) {
+        $data = json_decode($response, true);
+        $this->auth->storeToken($data['jwt']);
+        $this->headers['Authorization'] = 'Bearer ' . $data['jwt'];
       }
     }
+  }
 
-    public function setAuthHeaders($token){
-      $this->headers['Authorization'] = 'Bearer ' . $token;
-    }
-    public function getMessages()
-    {
-      $url = 'v1/messages?offset='.$this->offset.'&limit='. $this->limit.'&sort='. $this->sort
-      .'&datePublishBegin='.$this->datePublishBegin.'&datePublishEnd='.$this->datePublishEnd;
-      
-      $response = $this->apiRequest("GET", $url);
-      $data = json_decode($response, true);
-      return $data;
-    }
+  public function setAuthHeaders($token)
+  {
+    $this->headers['Authorization'] = 'Bearer ' . $token;
+  }
+  public function getMessages()
+  {
+    $url = 'v1/messages?offset=' . $this->offset . '&limit=' . $this->limit . '&sort=' . $this->sort
+      . '&datePublishBegin=' . $this->datePublishBegin . '&datePublishEnd=' . $this->datePublishEnd
+      . '&type=' . $this->messagesType;
 
-    public function getMessage($id)
-    {
-      $url = 'v1/messages/'.$id;
-      $response = $this->apiRequest("GET", $url);
-      $data = json_decode($response, true);
-      return $data;
-    }
+    $response = $this->apiRequest("GET", $url);
+    $data = json_decode($response, true);
+    return $data;
+  }
 
-    public function getFiles($messageId)
-    {
-      $url = 'v1/messages/'.$messageId.'/files/archive';
-      $response = $this->apiRequest("GET", $url);
-      $data = json_decode($response, true);
-      return $data;
-    }
+  public function getArbitralDecree()
+  {
+    $this->type = "ArbitralDecree";
+    return $this->getMessages();
+  }
 
-    public function setYesterdayDate(){
-      $this->datePublishBegin = date('Y-m-d', strtotime('-1 day'));
-      $this->datePublishEnd = date('Y-m-d');
-    }
+  public function getCompletionOfExtrajudicialBankruptcy()
+  {
+    $this->type = "CompletionOfExtrajudicialBankruptcy";
+    return $this->getMessages();
+  }
+
+  public function getMessage($id)
+  {
+    $url = 'v1/messages/' . $id;
+    $response = $this->apiRequest("GET", $url);
+    $data = json_decode($response, true);
+    return $data;
+  }
+
+  public function getFiles($messageId)
+  {
+    $url = 'v1/messages/' . $messageId . '/files/archive';
+    $response = $this->apiRequest("GET", $url);
+    $data = json_decode($response, true);
+    return $data;
+  }
+
+  public function setYesterdayDate()
+  {
+    $this->datePublishBegin = date('Y-m-d', strtotime('-1 day'));
+    $this->datePublishEnd = date('Y-m-d');
+  }
 }
