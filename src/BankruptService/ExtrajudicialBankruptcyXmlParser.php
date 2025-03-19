66c940d7114a673e038d57eec2e7d01e81ecddf9
vvvizard <vvvizard@gmail.com>
2025-03-20 01:15:27 +0700

    added base methods + type const

--- src/BankruptService/ExtrajudicialBankruptcyXmlParser.php
+++ src/BankruptService/ExtrajudicialBankruptcyXmlParser.php
@@ -6,10 +6,26 @@ use FedResSdk\BankruptService\XmlCardParser;
 
 class  ExtrajudicialBankruptcyXmlParser extends XmlCardParser
 {
-
+    public const TYPE = 'CompletionOfExtrajudicialBankruptcy';
+   
     public function __construct($xml)
     {
         parent::__construct($xml);
-    }   
+    }  
     
+    public function getNumber(){
+        return (string) $this->getMessageInfo()->CompletionOfExtrajudicialBankruptcy->StartOfExtrajudicialBankruptcyMessageNumber;
+    }
+
+    public function getText(){
+        return (string) $this->getMessageInfo()->CompletionOfExtrajudicialBankruptcy->Text;
+    }
+
+    public function getBankrupt(){
+        return  $this->xml->Bankrupt;
+    }
+
+    public function getBirthDate(){
+        return (string) $this->getMessageInfo()->BirthDate;
+    }
 }
